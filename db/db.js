fc18bdf5de018cb9d324fc4408159bbb062cb156
luka7711 <ulish6737@gmail.com>
2022-11-14 11:59:13 -0600

    Refactored db connection code

--- db/db.js
+++ db/db.js
@@ -1,24 +1,13 @@
 // This is where we will set up our db connection
 const mongoose = require("mongoose");
-const MongoClient = require("mongodb").MongoClient;
-
 const url = process.env.MONDGODB_URL;
 
-// that is automatically created
-mongoose
-  .connect(url, 
+
+mongoose.connect(url, 
     { 
       useNewUrlParser:true,
       useUnifiedTopology: true 
     }
   )
-  .then(
-    () => {
-      console.log("mongoose is connected")
-    }
-  )
-  .catch(
-    (err) => {
-      console.log(err, "something went wrong")
-    }
-  );
+  .then(() => console.log("mongoose is connected"))
+  .catch(err => console.log(err, "something went wrong"));
