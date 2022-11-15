0e7b57a481a4e4843510782311a6f52473b79148
luka7711 <ulish6737@gmail.com>
2022-11-14 20:50:44 -0600

    Created an API route that sends city and state

--- controllers/city.js
+++ controllers/city.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-const axios = require("axios");
+const City = require('../models/city');
 
+router.get("/:zipcode", async(req, res) => {
 
+    try {
+        const city = await City.findOne({ zipcodes: req.params.zipcode });
+        
+        if (city) {
+            res.status(200)
+            .json({
+                city: city['city'],
+                state_id: city['state_id']
+            })
+
+        } else {
+            res.status(200).
+            json(null)
+        }
+
+    } catch(error) {
+        console.log(error)   
+    } 
+})
 
 module.exports = router;
