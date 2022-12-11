45936ace57b6c63a654284957951fd1bb0c8cac3
luka7711 <ulish6737@gmail.com>
2022-12-11 17:24:47 -0600

    Add alternative port for deployment

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 require("dotenv").config();
 require("./db/db");
 
-const PORT = process.env.PORT;
+let PORT = process.env.PORT;
+
+if (PORT == null || PORT == "") PORT = 9000;
+
 const userController = require("./controllers/user");
 const cityController = require("./controllers/city");
 
@@ -16,6 +19,6 @@ app.use("/auth", userController);
 app.use("/cities", cityController);
 
 
-app.listen(PORT || 9000, () => {
+app.listen(PORT = () => {
   console.log("Listening on port 9000");
 });
